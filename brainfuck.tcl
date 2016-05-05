06992ed14355681bdfa5157234adfb2cfd7c5017
Fernando Raya <fernandoraya@gmail.com>
2016-05-05 10:20:12 +0200

    Run method changed and now have two parameters.

--- brainfuck.tcl
+++ brainfuck.tcl
@@ -3,6 +3,7 @@ lappend auto_path .
 package require fileutil
 package require brainfuck
 
+
 if {$argc < 1} {
     puts "Usage: tclsh brainfuck.tcl <brainfuck-program>"
     exit 1
@@ -13,13 +14,21 @@ if {$argc < 1} {
 #
 
 set filename [lindex $argv 0]
+
 if {![fileutil::test $filename fer msg]} {
     puts stderr "Error: $msg"
     exit 1
 }
 
 try {
-    brainfuck::run [brainfuck::load $filename]
+
+    set program  [brainfuck::read_program $filename]
+    set brackets [brainfuck::make_index_brackets $program]
+    
+    brainfuck::run $program $brackets 
+
+    exit 0
+    
 } on error {e} {
     puts $e
     exit 1
